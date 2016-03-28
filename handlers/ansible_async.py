1e6a42e9b38d4d9a21ac9640eab3450c268958d4
yumaojun <719118794@qq.com>
2016-03-28 21:51:32 +0800

    drop out celery, use threadpool.

--- handlers/ansible_async.py
+++ handlers/ansible_async.py
@@ -1,16 +1,19 @@
 # -*- coding:utf-8 -*-
 from __future__ import unicode_literals
 
-import tcelery
 import json
 
 import logging
 from tornado import gen
 from tornado.web import HTTPError
 from tornado.web import RequestHandler, asynchronous
-from libs import tasks
+from tornado.concurrent import run_on_executor
+from concurrent.futures import ThreadPoolExecutor
 
-tcelery.setup_nonblocking_producer()
+
+from libs.my_ansible_api import Ad_Hoc
+from multiprocessing import cpu_count
+
 logger = logging.getLogger('tornado.app')
 
 
@@ -20,6 +23,8 @@ class MainHandler(RequestHandler):
 
 
 class CommandHandler(RequestHandler):
+    executor = ThreadPoolExecutor(cpu_count())
+
     @asynchronous
     @gen.coroutine
     def post(self):
@@ -28,12 +33,29 @@ class CommandHandler(RequestHandler):
         except ValueError as e:
             raise HTTPError(400, e.message)
         logger.debug("input data: %s" % data)
-        response = yield gen.Task(tasks.exec_command.apply_async, args=[data.get("resource"), data.get("command")])
-        self.write(str(response.result))
+        response = yield self.exec_command(data.get("resource"), data.get("command"))
+        self.write(str(response))
         self.finish()
+
+    @run_on_executor
+    def exec_command(self, resource, command):
+        """
+        use ansible shell module to execute command on inventory.
+
+        Args:
+            resource: inventory resource, see Resource Class
+            command: which command you want to execute
+        Returns:
+            AnsibleReuslt: AnsibleResult instance, contain the all ansible return information.
+        """
+        res = Ad_Hoc(resource)
+        result = res.run(command, 'shell')
+        return result.result_deal
 
 
 class AdHocHandler(RequestHandler):
+    executor = ThreadPoolExecutor(cpu_count())
+
     @asynchronous
     @gen.coroutine
     def post(self):
@@ -46,7 +68,21 @@ class AdHocHandler(RequestHandler):
         if not(resource and module_name and module_arg):
             raise HTTPError(400, reason="resource and module_name and module_arg are required.")
 
-        response = yield gen.Task(tasks.ad_hoc.apply_async, args=[resource, module_name, module_arg])
-        self.write(str(response.result))
+        response = yield self.ad_hoc(resource, module_name, module_arg)
+        self.write(str(response))
         self.finish()
+
+    @run_on_executor
+    def ad_hoc(self, resource, module_name, module_arg):
+        """
+        执行ansible hoc-ad
+
+        Args:
+            resource:  ditto
+            module_name: ditto
+            module_arg: ditto
+        """
+        res = Ad_Hoc(resource)
+        result = res.run(module_arg, module_name)
+        return result.result_deal
 
